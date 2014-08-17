488a39d2b011c0ff946554e8df6c94ea3cd0ca1b
Joseph Vincent Tan <jvc_tan16@yahoo.com>
2014-08-17 12:31:36 +0800

    corrected db

--- DB Creator.java
+++ DB Creator.java
@@ -10,24 +10,22 @@ public class Main {
     public static void main(String[] args) {
       String DBcreation= ("create database if not exists embit;");
       String DBuse = ("USE embit;");
+	  
+      String DBtab_Fuser = ("CREATE TABLE IF NOT EXISTS Fuser ("
+            +"FuserCode CHAR(5) NOT NULL,"
+            +"FName VARCHAR(45) NOT NULL,"
+            +"address VARCHAR(45) NOT NULL,"
+            +"Contact INTEGER(10) UNSIGNED NOT NULL,"
+            +"Email VARCHAR(25) NOT NULL,"
+            +"PRIMARY KEY (FuserCode));");
+			
       String DBtab_user = ("CREATE TABLE IF NOT EXISTS user ("
             +"userCode CHAR(5) NOT NULL,"
-            +"userName VARCHAR(45) NOT NULL,"
-            +"address VARCHAR(45) NOT NULL,"
-            +"status CHAR(1) NOT NULL,"
-            +"dependent INTEGER(1) UNSIGNED NOT NULL,"
-            +"familyHead CHAR(1) NOT NULL,"
-            +"salary INTEGER(10) UNSIGNED NOT NULL,"
-            +"position VARCHAR(25) NOT NULL,"
+            +"Name VARCHAR(45) NOT NULL,"
+            +"Contact INTEGER(10) UNSIGNED NOT NULL,"
+            +"Email VARCHAR(25) NOT NULL,"
             +"PRIMARY KEY (userCode));");
-       String DBtab_log = ("CREATE TABLE IF NOT EXISTS timelog ("
-            +"userCode CHAR(5) NOT NULL,"
-            +"date DATE NOT NULL,"
-            +"timeIn TIME NOT NULL,"
-            +"timeOut TIME NOT NULL,"
-            +"remarks VARCHAR(45),"
-            +"PRIMARY KEY (userCode));");
-      try {
+       try {
             Class.forName ("com.mysql.jdbc.Driver");
             dbconnection = DriverManager.getConnection("jdbc:mysql://localhost:3306/","root","1234");
             PreparedStatement ps = dbconnection.prepareStatement(DBcreation);
@@ -36,13 +34,15 @@ public class Main {
             ps.executeQuery();
             JOptionPane.showMessageDialog(frame,"embit database is set","Database",JOptionPane.INFORMATION_MESSAGE);
             
-            ps = dbconnection.prepareStatement(DBtab_user);
+            ps = dbconnection.prepareStatement(DBtab_Fuser);
+            ps.executeUpdate();
+            JOptionPane.showMessageDialog(frame,"Table 'Fuser' is set","Database",JOptionPane.INFORMATION_MESSAGE);
+            
+			ps = dbconnection.prepareStatement(DBtab_user);
             ps.executeUpdate();
             JOptionPane.showMessageDialog(frame,"Table 'user' is set","Database",JOptionPane.INFORMATION_MESSAGE);
             
-            ps = dbconnection.prepareStatement(DBtab_log);
-            ps.executeUpdate();
-            JOptionPane.showMessageDialog(frame,"Table 'timetable' is set","Database",JOptionPane.INFORMATION_MESSAGE);
+            
             ps.close();
             dbconnection.close();
             }
